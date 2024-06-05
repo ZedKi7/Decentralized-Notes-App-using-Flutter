d102fcce91952a515842079f41db32efc864e93c
ZedKi7 <z.babaousmail@esi-sba.dz>
2024-06-05 16:09:58 +0100

    Initialize the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# dapp_flutter_blockchain
+
+DApp Using Flutter | Blockchain | Web 3 | Solidity
---
+++ android/app/src/main/kotlin/com/zedki7/dapp_flutter_blockchain/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.zedki7.dapp_flutter_blockchain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
